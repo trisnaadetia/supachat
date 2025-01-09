5adaacb8e584654619ab15d6b37a33bf0cef7c90
trisnaadetia <trisnaadetia@gmail.com>
2025-01-09 10:58:23 +0700

    init tailwind and shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,5 +11,10 @@ export default defineConfig(({ mode }) => {
       'process.env.SUPABASE_ANON_KEY': JSON.stringify(env.SUPABASE_ANON_KEY)
     },
     plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   }
 })
